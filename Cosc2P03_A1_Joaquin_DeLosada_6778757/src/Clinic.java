b1349c30d91793917a70bcdd9d38a40fbbfbdd78
Joaquin De Losada <44423718+Montainproductions@users.noreply.github.com>
2022-09-15 13:28:34 -0400

    Setting up file reader
    Java now should read the file but the file isnt being found for some reason. Also expanding code for timer and to go to the next line.

--- Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Clinic.java
+++ Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Clinic.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 public class Clinic {
     public static void RunClinic(){
         //Timer();
@@ -6,12 +7,41 @@ public class Clinic {
     }
 
     public static void Timer(){
+        int currentTime = 900;
+        for(int i = 0; i <= 960; i++){
+            if(i%2 == 0){
+                currentTime++;
+            }
+            System.out.println(currentTime);
+        }
 
+        int vaccineTimer = 0;
+        for(int i = 0; i <= 15; i++){
+            vaccineTimer++;
+            System.out.println(vaccineTimer);
+        }
     }
 
     public static void ReadData(){
-        File file = new File("C:\\Users\\monta\\Documents\\GitHub\\Java-2P03-Assignemnts\\Cosc2P03_A1_Joaquin_DeLosada_6778757\\src\\patients.txt");
-        System.out.println(file);
+        File txtFile = new File("C:\\Users\\monta\\Documents\\GitHub\\Java-2P03-Assignemnts\\Cosc2P03_A1_Joaquin_DeLosada_6778757\\src\\patients.txt");
+        System.out.println(txtFile);
+
+        BufferedReader readFile;
+        //"patients.txt"
+        readFile = new BufferedReader(new FileReader(txtFile.getName())); //Read file in theory
+        String line = "";
+        while(line != null /*|| currentTime > "06:00"*/){
+            line = readFile.readLine();
+            String[] currentPatient = line.Split("  ", 6);
+            //Patient[]
+            System.out.print(currentPatient);
+            /*if(currentPatient[5] <= currentTime){
+                Patient newPatient = New Patient();
+
+            }*/
+        }
+        //System.out.print(line);
+        //readFile.close();
 
         //Sudo code
         /*for(string array[] line in file){
@@ -28,7 +58,7 @@ public class Clinic {
             }
         }
 
-         */
+        */
     }
 
     public static void main(String[] args) {
