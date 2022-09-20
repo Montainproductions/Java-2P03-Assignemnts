a8b62252b9b318d2f7ca31c8f32e587d30da4f88
Joaquin De Losada <44423718+Montainproductions@users.noreply.github.com>
2022-09-20 10:41:17 -0400

    File is not reading
    Aperrently I needed a add a try catch for it to work. Also started to work on the rest of the code

--- Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Clinic.java
+++ Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Clinic.java
@@ -2,29 +2,29 @@ import java.io.*;
 
 public class Clinic {
     public static void RunClinic(){
-        InternalTimer timer = new InternalTimer();
-        //timer.StartDay();
         ReadData();
+        InternalTimer timer = new InternalTimer();
+        timer.StartDay();
     }
 
     public static void ReadData(){
-        File txtFile = new File("recourse//patients.txt");
-        System.out.println(txtFile.exists());
-
+        File txtFile = new File("recourses//Patients.txt");
         BufferedReader readFile;
-        //"patients.txt"
-        //readFile = new BufferedReader(new FileReader("recourses//patients.txt")); //Read file in theory
-        String line = "";
-        /*while(line != null /*|| currentTime > "06:00"){
-            line = readFile.readLine();
-            String[] currentPatient = line.Split("  ", 6);
-            //Patient[]
-            System.out.print(currentPatient);
-            /*if(currentPatient[5] <= currentTime){
-                Patient newPatient = New Patient();
-
+        try {
+            readFile = new BufferedReader(new FileReader(txtFile)); //Read file in theory
+            String line = "";
+            //System.out.println(readFile);
+            while(line != null /*|| currentTime > "06:00"*/){
+                line = readFile.readLine();
+                String[] currentPatient = line.split("  ");
+                //Patient[]
+                System.out.print(currentPatient);
             }
-        }*/
+        } catch(FileNotFoundException e){
+            System.out.println("File not found. Did you try to move it? Not a good idea return it.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         //System.out.print(line);
         //readFile.close();
 
--- Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Patient.java
+++ Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Patient.java
@@ -1,4 +1,4 @@
-public abstract class Patient {
+public class Patient {
 
     String patientName;
     int patientAge;
