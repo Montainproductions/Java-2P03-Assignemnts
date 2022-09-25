961f4db925f6c97f632a006afe154ef03e190e77
Joaquin De Losada <44423718+Montainproductions@users.noreply.github.com>
2022-09-25 12:55:01 -0400

    Improved linked list
    I expanded the link list and the Node classes acording to the book and now I am going to make it work for double link list

--- Cosc2P03_A1_Joaquin_DeLosada_6778757/src/LinkedList.java
+++ Cosc2P03_A1_Joaquin_DeLosada_6778757/src/LinkedList.java
@@ -4,7 +4,52 @@ public class LinkedList {
         head = null;
     }
 
-    public void AddtoStart(){
+    public void AddtoStart(Patient newPatient, int count){
+        head = new Node(newPatient,null, count, head);
+    }
 
+    public boolean DeleteHeadNode(){
+        if(head != null){
+            head = head.GetNextNode();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int Size(){
+        int count = 0;
+        Node position = head;
+        while(position != null){
+            count++;
+            position = position.GetNextNode();
+        }
+        return count;
+    }
+
+    public boolean Contains(Patient patient){
+        return (Find(patient) != null);
+    }
+
+    private Node Find(Patient target){
+        Node position = head;
+        Patient patientAtNode;
+        while(position != null){
+            patientAtNode = position.currentPatient;
+            if(patientAtNode.equals(target)){
+                return position;
+            }
+            position = position.GetNextNode();
+        }
+        return null;
+    }
+
+    public void OutputList(){
+        Node position = head;
+        while(position != null){
+            System.out.println(position.GetCurrentPatient() + " " + position.GetCount());
+
+            position.GetNextNode();
+        }
     }
 }
--- Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Node.java
+++ Cosc2P03_A1_Joaquin_DeLosada_6778757/src/Node.java
@@ -1,23 +1,30 @@
 public class Node {
+    public Patient currentPatient;
     public Node previousNode; //Previous node
     public int count; //Current Position
     public Node nextNode; //Next node
     public Node(){
+        currentPatient = null;
         previousNode = null;
         count = 0;
         nextNode = null;
     }
-    public Node(Node newP, int newI, Node newN){
+    public Node(Patient newPatient, Node newP, int newCount, Node newN){
+        currentPatient = newPatient;
         previousNode = newP;
-        count = newI;
+        count = newCount;
         nextNode = newN;
     }
 
-    public Node getNextNode() {
+    public Patient GetCurrentPatient(){return currentPatient;}
+
+    public Node GetNextNode() {
         return nextNode;
     }
 
     public Node getPreviousNode() {
         return previousNode;
     }
+
+    public int GetCount(){return count;}
 }
