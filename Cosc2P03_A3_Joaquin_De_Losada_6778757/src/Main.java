6477c9f834ad22c4b9b029c8fdeffb92a1b3ad6c
Joaquin De Losada <44423718+Montainproductions@users.noreply.github.com>
2022-10-28 10:16:12 -0400

    Setting up Assignment 3

---
+++ Cosc2P03_A3_Joaquin_De_Losada_6778757/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+//readdata, trickleDown, buildHeap, removeMin, inordertraverse, heapSort
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
